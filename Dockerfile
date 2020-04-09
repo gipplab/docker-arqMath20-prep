ca3f9b73b6cf6db967443abf3188a2d585a57143
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-04-10 00:13:20 +0200

    Test locally
    * change HTML cleaning library

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3
 
 WORKDIR /app
-COPY ./ARQMathCode/requirements.txt /app/requirements.txt
+COPY ./requirements.txt /app/requirements.txt
 RUN pip install -r requirements.txt
 COPY . /app
 COPY main.py /app
--- main.py
+++ main.py
@@ -1,35 +1,46 @@
 import csv
 import logging
-
+from bs4 import BeautifulSoup
 from ARQMathCode.post_reader_record import DataReaderRecord
 
-root = logging.getLogger()
-root.setLevel(logging.DEBUG)
 
-
-csvfile = open('/data/qa-pair.csv', mode='w')
-fieldnames = ['qId', 'aID', 'q', 'a', 'rel']
-writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-writer.writeheader()
-logging.info("Output file created.")
+def to_plain(html: str) -> str:
+    return ''.join(BeautifulSoup(html, 'html.parser').findAll(text=True))
 
 
 def main():
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO)
+    logger.setLevel(level=logging.DEBUG)
+    csvfile = open('/data/qa-pair.csv', mode='w')
+    fieldnames = ['qID', 'aID', 'q', 'a', 'rel']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    logger.info("Output file created.")
     dr = DataReaderRecord('/data')
     lst_questions = dr.get_question_of_tag("calculus")
     logging.info(f'{len(lst_questions)} questions for tag calculus')
     for q in lst_questions:
-        q_text = q.to_str()
-        answers = dr.get_answers_for_question(q)
-        logging.debug(f'Precessing {q.title} with {len(answers)} answers')
+        q_text = to_plain(q.title + '\n\n' + q.body)
+        answers = get_answer_list(dr, q)
+        logger.debug(f'Precessing {q.title} with {len(answers)} answers')
         for a in answers:
             writer.writerow({
                 'qID': q.post_id,
                 'aID': a.post_id,
                 'q': q_text,
-                'a': a.to_str(),
+                'a': to_plain(a.body),
                 'rel': q.accepted_answer_id == a.post_id
             })
+        csvfile.flush()
+    csvfile.close()
+
+
+def get_answer_list(dr, q):
+    answers = dr.get_answers_for_question(q.post_id)
+    if answers is None:
+        answers = []
+    return answers
 
 
 if __name__ == "__main__":

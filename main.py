6b81828e60beca4de7458052441fd19e639fb662
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-04-13 23:26:37 +0200

    Shrink dataset size even more

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 import logging
 import pickle
 from os import path
-from typing import TypeVar, Callable
+from typing import TypeVar, Callable, Dict, List
 
 from bs4 import BeautifulSoup
 
@@ -40,15 +40,71 @@ def pkl_write(pkl_location, data: T) -> T:
 
 
 def save_tag_subset(self: DataReaderRecord, tag):
+    logger = logging.getLogger(__name__)
+    logger.debug('map sizes %s %s', len(self.post_parser.map_questions), len(self.post_parser.map_answers))
+    user_whitelist = {}
+    duplicate_whitelist = {}
+    related_whitelist = {}
+    keep_questions = self.get_question_of_tag(tag)
+    keep_qid = []
+
+    def add_user(uid):
+        if uid is None:
+            user_whitelist[-1] = self.user_parser.map_of_user[-1]
+        else:
+            current_user = self.user_parser.map_of_user.get(uid)
+            if current_user:
+                user_whitelist[uid] = current_user
+            else:
+                logger.warning(f'user {uid} not mapped')
+
+    def add_users(quest_id):
+        for c in self.comment_parser.map_of_comments_for_post.get(quest_id, []):
+            add_user(c.user_id)
+        for v in self.vote_parser.map_of_votes.get(quest_id, []):
+            add_user(v.user_id)
+        for answer in answers:
+            add_user(answer.owner_user_id)
+            add_user(answer.last_editor_user_id)
+            for c in self.comment_parser.map_of_comments_for_post.get(answer.post_id, []):
+                add_user(c.user_id)
+            for v in self.vote_parser.map_of_votes.get(answer.post_id, []):
+                add_user(v.user_id)
+
+    def insert_related(d: Dict[str, List[str]], k, l: List, wl: Dict):
+        if d.get(k):
+            keep_qid.extend(d.get(k))
+            wl[k] = d.get(k)
+        for key, value in d.items():
+            if k in value:
+                keep_qid.extend(value)
+                wl[key] = d.get(key)
+
+    for question in list(keep_questions):
+        keep_qid.append(question.post_id)
+        insert_related(self.post_link_parser.map_duplicate_posts, question.post_id, keep_questions, duplicate_whitelist)
+        insert_related(self.post_link_parser.map_related_posts, question.post_id, keep_questions, related_whitelist)
+    self.post_link_parser.map_related_posts = related_whitelist
+    self.post_link_parser.map_duplicate_posts = duplicate_whitelist
+
     for question_id in list(self.post_parser.map_questions):
         question = self.post_parser.map_questions[question_id]
-        lst_tags = question.tags
-        if tag not in lst_tags:
-            l = get_answer_list(self, question)
-            for a in l:
-                self.post_parser.map_answers.pop(a.post_id, None)
+        answers = get_answer_list(self, question)
+        if question_id not in keep_qid:
+            for a in answers:
                 self.post_parser.map_just_answers.pop(a.post_id, None)
+                self.comment_parser.map_of_comments_for_post.pop(a.post_id, None)
+                self.vote_parser.map_of_votes.pop(a.post_id, None)
+            self.post_parser.map_answers.pop(question_id, None)
             self.post_parser.map_questions.pop(question_id, None)
+            self.comment_parser.map_of_comments_for_post.pop(question_id, None)
+            self.vote_parser.map_of_votes.pop(question_id, None)
+            logger.debug('map sizes %s %s', len(self.post_parser.map_questions),
+                         len(self.comment_parser.map_of_comments_for_post))
+        else:
+            add_users(question_id)
+
+    self.user_parser.map_of_user = user_whitelist
 
 
 def main():

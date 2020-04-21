1ab13e470917463694827d06d32d6f5482dc9c70
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-04-21 15:00:09 +0200

    Improve performance

--- pywikibot/user-config.py
+++ pywikibot/user-config.py
@@ -2,3 +2,6 @@ mylang='en'
 family = 'fse'
 usernames['fse']['fse'] = u'SchuBot'
 family_files['fse'] = 'https://arq20.formulasearchengine.com/api.php'
+maxthrottle = 2
+put_throttle = 1
+maxlag = 0

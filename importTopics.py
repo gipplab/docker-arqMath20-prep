18d67ac984c2973afca03cde623ce03e84d64a7e
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-04-22 19:45:33 +0200

    Generate topic overview page

--- importTopics.py
+++ importTopics.py
@@ -1,5 +1,7 @@
 import asyncio
 
+import pywikibot.flow
+
 from Topics.TopicReader import TopicReader
 from Wikidata.Wikidata import Wikidata
 from Wikitext.Wikitext import Wikitext
@@ -8,15 +10,26 @@ formula_reader = TopicReader('/data/Topics_V1.1.xml')
 text_reader = TopicReader('/data/Topics_V2.0.xml')
 wd = Wikidata()
 
-texts = []
+tasks = []
+
+board = pywikibot.flow.Board(wd.get_site(), "Formula Topics")
+
+
+async def addTopic(tid, text, wt):
+    wt = await wt.to_wikitext(text)
+    board.new_topic(f'Topic {tid}', wt)
+
+
 for k, v in formula_reader.map_topics.items():
     wt = Wikitext(wd)
     wt.highlight = [v.formula]
-    text = texts.append(wt.to_wikitext(v.title + v.question))
+    text=f"== {v.title} == \n\n{v.question}"
+    t= addTopic(k, text, wt)
+    tasks.append(t)
 
 
 async def run():
-    results = await asyncio.gather(*texts)
+    results = await asyncio.gather(*tasks)
     for res in results:
         if 0 > res.find('highlight'):
             print(res)
